3563a04ddecfc0eff730c0e800f073376cc25e7d
nsajic <nikola9n@gmail.com>
2017-06-18 23:52:56 +0200

    konflikti

--- bsepproj/src/main/java/com/bsep/proj/controller/LogRegKontroler.java
+++ bsepproj/src/main/java/com/bsep/proj/controller/LogRegKontroler.java
@@ -8,6 +8,7 @@ import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
@@ -59,6 +60,4 @@ public class LogRegKontroler {
 			return new ResponseEntity<Poruka>(new Poruka("NijeIzlogovan", null), HttpStatus.ACCEPTED);			
 		}
 	}
-	
-	
 }
--- bsepproj/src/main/resources/data.sql
+++ bsepproj/src/main/resources/data.sql
@@ -1,16 +1,14 @@
 insert into rola (naziv) values ('Admin');
-insert into rola (naziv) values ('PravnoLice');
-insert into rola (naziv) values ('MenadzerSistema');
+insert into rola (naziv) values ('Pravsafsa');
 
-insert into privilegija (naziv) values ('Banka:Dodaj');
-insert into privilegija (naziv) values ('Banka:Izmeni');
---insert into privilegija (naziv) values ('Banka:Obrisi');
-insert into privilegija (naziv) values ('Banka:IzlistajPretrazi');
 insert into privilegija (naziv) values ('SelfSign');
+insert into privilegija (naziv) values ('CA');
+insert into privilegija (naziv) values ('5');
+
+
 
 insert into rola_privilegija values (1, 1);
 insert into rola_privilegija values (1, 2);
 insert into rola_privilegija values (1, 3);
-insert into rola_privilegija values (1, 4);
 
 insert into korisnik (tip_korisnika, ime, prezime, rola_id, email, sifra, username) values ('A' ,'Sima1', 'Simic1', 1, 'k1@k1.com', '$2a$12$/eDioOpiKrGQnDv53hPxIe8XDCJWP/Xe1fF6E5IrTVxuBozu9EX9G', 'kor');
